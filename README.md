ac2c6a27e620573dfbc3cdd07dab09d3f52ae3b9
Leomelonseeds <leonardfang15@gmail.com>
2024-06-11 21:25:31 -0700

    Project creation

---
+++ README.md
@@ -0,0 +1,2 @@
+# Ultima Particles
+This plugin uses the [PlayerParticles](https://www.spigotmc.org/resources/playerparticles.40261/) API to provide individual, easily selectable particle styles for Ultima.
---
+++ src/main/java/com/leomelonseeds/ultimaparticles/UltimaParticles.java
@@ -0,0 +1,14 @@
+package com.leomelonseeds.ultimaparticles;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class UltimaParticles extends JavaPlugin {
+    
+    private static UltimaParticles plugin;
+    
+    @Override
+    public void onEnable() {
+        plugin = this;
+    }
+    
+}
